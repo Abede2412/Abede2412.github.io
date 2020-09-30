f07904e834500f91d012c9ddf27bdac48a7a178f
Abede2412 <67693937+Abede2412@users.noreply.github.com>
2020-09-30 10:58:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const tombol_kontak = document.querySelector('.tombol-kontak');
+const sosmed = document.querySelector('.sosmed')
+
+tombol_kontak.addEventListener('click', function(event){
+  sosmed.style.display = 'block';
+});
